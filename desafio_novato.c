901e242fd74321a235e518e7a13a6e6da6d8fee0
Marcos Henrique <129520104+milerHDbrk@users.noreply.github.com>
2025-10-09 22:39:33 -0300

    Projeto V2

--- desafio_novato.c
+++ desafio_novato.c
@@ -6,7 +6,7 @@ int main () {
 
     char estado1, estado2;
     char codigo_estado1[3], codigo_estado2[3];
-    char nome_cidade1[3], nome_cidade2[3];
+    char nome_cidade1[20], nome_cidade2[20];
     int populacao1, populacao2;
     float area1, area2;
     float pib1, pib2;
@@ -17,7 +17,7 @@ int main () {
 
 printf("Carta 1: \n");
 
-    printf("Digite uma Letra de A - H (carta 1): \n");
+    printf("Digite uma letra de A - H (carta 1): \n");
         scanf("%s", &estado1); // estado A - H (carta 1)
 
     printf("Digite o Código da carta 1 (carta 1): \n");
@@ -29,20 +29,20 @@ printf("Carta 1: \n");
     printf("Digite a quantidade populacional (carta 1): \n");
         scanf("%d", &populacao1); // população total da cidade (carta 1)
 
-    printf("Digite a area (carta 1): \n");
+    printf("Digite a área (carta 1): \n");
         scanf("%f", &area1); // área da cidade (carta 1)
 
-    printf("Digite o pib (carta 1): \n");
+    printf("Digite o PIB (carta 1): \n");
         scanf("%f", &pib1); // produto interno bruto da cidade (carta 1)
 
-    printf("Digite o número de pontos turisticos (carta 1): \n");
-        scanf("%f", &num_pontos_T1); // número de pontos turisticos da cidade (carta 1)
+    printf("Digite o número de pontos turísticos (carta 1): \n");
+        scanf("%f", &num_pontos_T1); // número de pontos turísticos da cidade (carta 1)
 
 // INSERIR INFORMAÇÕES A CARTA 2
 
 printf("Carta 2: \n");
 
-    printf("Digite uma Letra de A - H (carta 2): \n");
+    printf("Digite uma letra de A - H (carta 2): \n");
         scanf("%s", &estado2); // estado A - H (carta 2)
 
     printf("Digite o Código da carta 1 (carta 2): \n");
@@ -54,14 +54,14 @@ printf("Carta 2: \n");
     printf("Digite a quantidade populacional (carta 2): \n");
         scanf("%d", &populacao2); // população total da cidade (carta 2)
 
-    printf("Digite a area (carta 2): \n");
+    printf("Digite a área (carta 2): \n");
         scanf("%f", &area2); // área da cidade (carta 2)
 
-    printf("Digite o pib (carta 2): \n");
+    printf("Digite o PIB (carta 2): \n");
         scanf("%f", &pib2); // produto interno bruto da cidade (carta 2)
 
-    printf("Digite o número de pontos turisticos (carta 2): \n");
-        scanf("%f", &num_pontos_T2); // número de pontos turisticos da cidade (carta 2)
+    printf("Digite o número de pontos turísticos (carta 2): \n");
+        scanf("%f", &num_pontos_T2); // número de pontos turísticos da cidade (carta 2)
 
 return 0;
 }
